397fa918212237f203df47be90e15c7b07619c7f
Madhav Mathur <madhavmathur@outlook.com>
2024-03-24 19:02:10 -0400

    make pdf links https

--- app/(deals)/deal_details/modals/PaymentContent.js
+++ app/(deals)/deal_details/modals/PaymentContent.js
@@ -4,7 +4,7 @@ const PaymentContent = () => (
   <div>
     <p>Details about the Payment...</p>
     <iframe
-      src="http://docs.google.com/gview?url=https://slicedinvoices.com/pdf/wordpress-pdf-invoice-plugin-sample.pdf&embedded=true"
+      src="https://docs.google.com/gview?url=https://slicedinvoices.com/pdf/wordpress-pdf-invoice-plugin-sample.pdf&embedded=true"
       width="100%"
       height="600px"
       style={{ border: 'none' }}
--- app/(deals)/deal_details/modals/SignOffContent.js
+++ app/(deals)/deal_details/modals/SignOffContent.js
@@ -4,7 +4,7 @@ const SignOffContent = () => (
   <div>
     <p>Details about the Sign Off...</p>
     <iframe
-      src="http://docs.google.com/gview?url=https://www.sxsw.com/wp-content/uploads/2019/03/Master-Use-Synch-License-Template.pdf&embedded=true"
+      src="https://docs.google.com/gview?url=https://www.sxsw.com/wp-content/uploads/2019/03/Master-Use-Synch-License-Template.pdf&embedded=true"
       width="100%"
       height="600px"
       style={{ border: 'none' }}
