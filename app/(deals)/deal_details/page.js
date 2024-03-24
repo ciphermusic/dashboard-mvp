27dd1e4e132a0f04392b213910431f9eeb9edbed
Madhav Mathur <madhavmathur@outlook.com>
2024-03-24 18:45:29 -0400

    Add lots of styling to deals

--- app/(deals)/deal_details/page.js
+++ app/(deals)/deal_details/page.js
@@ -9,6 +9,7 @@ import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timel
 import DealTimelineElementType from 'data/types/DealTimelineElementType';
 import ModalContent from "./modals/ModalContent";
 import { Modal, Button } from 'react-bootstrap';
+import { VectorPen, CreditCardFill, GearWideConnected, SendCheck, HandThumbsUpFill } from "react-bootstrap-icons";
 
 import 'react-vertical-timeline-component/style.min.css';
 
@@ -62,34 +63,6 @@ const Deals = () => {
                   </div>
               </div>
           </Col>
-          </Row>
-          <Row className="justify-content-center">
-              <Col xl={4} lg={6} md={12} xs={12} className="mt-6" key={"abc"}>
-                <Card>
-                    <Card.Body>
-                        <div className="d-flex justify-content-between align-items-center mb-3">
-                            <div>
-                                <h4 className="mb-0">Requester Budget</h4>
-                            </div>
-                        </div>
-                        <div>
-                            <h1 className="fw-bold">$265k - 340k</h1>
-                            {/* <p className="mb-0" dangerouslySetInnerHTML={{ __html: info.statInfo}}></p> */}
-                        </div>
-                    </Card.Body>
-                </Card>
-              </Col>
-              <Col xl={4} lg={6} md={12} xs={12} className="mt-6" key={"abc"}>
-                <Card>
-                    <Card.Body>
-                        <div className="d-flex justify-content-between align-items-center mb-3">
-                            <div>
-                                <h4 className="mb-0">Total Stakeholders</h4>
-                            </div>
-                        </div>
-                    </Card.Body>
-                </Card>
-            </Col>
             <Row className="justify-content-center">
                 <Col xl={4} lg={6} md={12} xs={12} className="mt-6" key={"abc"}>
                   <Card>
@@ -160,12 +133,11 @@ const Deals = () => {
                   visible = {true}
                   className="vertical-timeline-element--work"
                   date="2011 - present"
-                  iconStyle={{ background: 'rgb(97,75,255,255)', color: '#fff' }}
+                  iconStyle={{ background: 'rgb(75,255,96,1)'}}
                   icon={
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-1-circle-fill" viewBox="0 0 16 16">
-                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M9.283 4.002H7.971L6.072 5.385v1.271l1.834-1.318h.065V12h1.312z"/>
-                    </svg>
+                    <SendCheck/>
                   }
+                  style={{ opacity: 0.6 }}
                 >
                 <button onClick={() => handleShowModal(DealTimelineElementType.REQUEST)} style={{ all: 'unset', cursor: 'pointer', width: '100%', color: 'inherit', background: 'inherit' }}>
                     <h3 className="vertical-timeline-element-title">Request Filed</h3>
@@ -179,12 +151,11 @@ const Deals = () => {
                   visible = {true}
                   className="vertical-timeline-element--work"
                   date="2010 - 2011"
-                  iconStyle={{ background: 'rgb(97,75,255,255)', color: '#fff' }}
+                  iconStyle={{ background: 'rgb(75,255,96,1)', border: '5px solid lightgreen', padding: '2px'}}
                   icon={
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-2-circle-fill" viewBox="0 0 16 16">
-                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.646 6.24c0-.691.493-1.306 1.336-1.306.756 0 1.313.492 1.313 1.236 0 .697-.469 1.23-.902 1.705l-2.971 3.293V12h5.344v-1.107H7.268v-.077l1.974-2.22.096-.107c.688-.763 1.287-1.428 1.287-2.43 0-1.266-1.031-2.215-2.613-2.215-1.758 0-2.637 1.19-2.637 2.402v.065h1.271v-.07Z"/>
-                    </svg>
+                    <HandThumbsUpFill/>
                   }
+                  style={{ opacity: 0.6 }}
                 >
                 <button onClick={() => handleShowModal(DealTimelineElementType.NEGOTIATION_MASTER)} style={{ all: 'unset', cursor: 'pointer', width: '100%', color: 'inherit', background: 'inherit' }}>
                   <h3 className="vertical-timeline-element-title">Negotiation - Master Owner</h3>
@@ -198,12 +169,11 @@ const Deals = () => {
                   visible = {true}
                   className="vertical-timeline-element--work"
                   date="2008 - 2010"
-                  iconStyle={{ background: 'rgb(97,75,255,255)', color: '#fff' }}
+                  iconStyle={{ background: 'rgb(75,255,96,1)', border: '5px solid lightgreen', padding: '2px'}}
                   icon={
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-3-circle-fill" viewBox="0 0 16 16">
-                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-8.082.414c.92 0 1.535.54 1.541 1.318.012.791-.615 1.36-1.588 1.354-.861-.006-1.482-.469-1.54-1.066H5.104c.047 1.177 1.05 2.144 2.754 2.144 1.653 0 2.954-.937 2.93-2.396-.023-1.278-1.031-1.846-1.734-1.916v-.07c.597-.1 1.505-.739 1.482-1.876-.03-1.177-1.043-2.074-2.637-2.062-1.675.006-2.59.984-2.625 2.12h1.248c.036-.556.557-1.054 1.348-1.054.785 0 1.348.486 1.348 1.195.006.715-.563 1.237-1.342 1.237h-.838v1.072h.879Z"/>
-                    </svg>
+                    <HandThumbsUpFill/>
                   }
+                  style={{ opacity: 0.6 }}
                 >
                 <button onClick={() => handleShowModal(DealTimelineElementType.NEGOTIATION_OTHER)} style={{ all: 'unset', cursor: 'pointer', width: '100%', color: 'inherit', background: 'inherit' }}>
                   <h3 className="vertical-timeline-element-title">Negotiation - Other Owners</h3>
@@ -217,11 +187,9 @@ const Deals = () => {
                   visible = {true}
                   className="vertical-timeline-element--work"
                   date="2008 - 2010"
-                  iconStyle={{ background: 'rgb(97,75,255,255)', color: '#fff' }}
+                  iconStyle={{ background: 'rgb(255,186,75,1)', border: '5px solid orange', padding: '2px' }}
                   icon={
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-4-circle-fill" viewBox="0 0 16 16">
-                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M7.519 5.057c-.886 1.418-1.772 2.838-2.542 4.265v1.12H8.85V12h1.26v-1.559h1.007V9.334H10.11V4.002H8.176zM6.225 9.281v.053H8.85V5.063h-.065c-.867 1.33-1.787 2.806-2.56 4.218"/>
-                    </svg>
+                    <VectorPen />
                   }
                 >
                 <button onClick={() => handleShowModal(DealTimelineElementType.SIGN_OFF)} style={{ all: 'unset', cursor: 'pointer', width: '100%', color: 'inherit', background: 'inherit' }}>
@@ -236,11 +204,9 @@ const Deals = () => {
                   visible = {true}
                   className="vertical-timeline-element--work"
                   date="2008 - 2010"
-                  iconStyle={{ background: 'rgb(97,75,255,255)', color: '#fff' }}
+                  iconStyle={{ background: 'rgb(255,186,75,1)', border: '5px solid orange', padding: '2px' }}
                   icon={
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-4-circle-fill" viewBox="0 0 16 16">
-                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M7.519 5.057c-.886 1.418-1.772 2.838-2.542 4.265v1.12H8.85V12h1.26v-1.559h1.007V9.334H10.11V4.002H8.176zM6.225 9.281v.053H8.85V5.063h-.065c-.867 1.33-1.787 2.806-2.56 4.218"/>
-                    </svg>
+                    <CreditCardFill />
                   }
                 >
                 <button onClick={() => handleShowModal(DealTimelineElementType.PAYMENT)} style={{ all: 'unset', cursor: 'pointer', width: '100%', color: 'inherit', background: 'inherit' }}>
