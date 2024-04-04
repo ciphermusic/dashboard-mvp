6049cf8113ad071ac970e3a5a0c9b40f5276e798
Madhav Mathur <madhavmathur@outlook.com>
2024-04-04 17:52:58 -0400

    add basic get call

---
+++ app/api/api_client.js
@@ -0,0 +1,31 @@
+import { supabase } from '../../data/utils/supabaseClient';
+
+const dealState = {
+	"New": 0,
+	"Active": 1,
+    "Await_Master": 2,
+    "Await_Publisher": 3,
+    "Await_Other": 4,
+    "Email_Master_Sent": 5,
+    "Email_Publisher_Sent": 6,
+    "Email_Others_Sent": 7,
+    "Contract_Sent": 8,
+    "Signed": 9,
+    "Payment_Sent": 10,
+    "Payment_Complete": 11,
+    "Done": 12,
+}
+
+export const getDealState = async () => {
+    const { data, error } = await 
+        supabase
+        .from('Deal')
+        .select('state');
+
+    if (error) {
+        console.log("Couldn't load deal state: " + error);
+    } else {
+        console.log("Deal state: " + data);
+        return dealState[data];
+    }
+}
