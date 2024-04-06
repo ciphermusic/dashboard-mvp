d71f0e9c8437988e362358c063ce5587fad57f30
Lucas Smulders <lucassmulders@dhcp-vl2053-511.cornell-visitor.cit.cornell.edu>
2024-04-06 18:43:53 -0400

    a lot of changes

--- app/(deals)/deal_details/modals/NegoMasterContent.js
+++ app/(deals)/deal_details/modals/NegoMasterContent.js
@@ -1,18 +1,74 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Container } from 'react-bootstrap';
+import { getUserId, postDealPrice, postDealState } from '../../../api/api_client' ;
 
-const NegoMasterContent = () => (
-  <div>
-    <p>Details about the Negotiation - Master Owner...</p>
-    <Container className="mt-5">
-      <form>
-        <div className="form-group">
-          <label htmlFor="priceInput">Price</label>
-          <input className="form-control" id="priceInput" placeholder="Enter price" />
+import Lottie from "lottie-react";
+import CheckMark from "../../../../public/images/animations/Checkmark.json";
+
+const NegoMasterContent = ({dealState, setDealState, setShowModal, setCurrentPrice}) => {
+  const [price, setPrice] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [showError, setShowError] = useState(false);
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!price) {
+      setShowError(true);
+    } else {
+      setShowError(false);
+      setFormSubmitted(true);
+
+      const user = await getUserId();
+      const dealPriceError = await postDealPrice({userId: user.id, price: price});
+      const dealStateError = await postDealState(4);
+
+      if (dealPriceError || dealStateError) {
+        console.log("Couldn't update deal price");
+      }
+
+      setTimeout(() => {
+        setCurrentPrice(price);
+        setDealState(4);
+        setShowModal(false);
+      }, 800);
+    }
+  }
+
+  return (
+    formSubmitted ? (
+      <Container className="mt-5">
+        <div style={{ 
+          display: 'flex', 
+          justifyContent: 'center', 
+          alignItems: 'center', 
+        }}>
+          <Lottie animationData={CheckMark} style={{ width: '25%', height: '25%' }} loop="false" />
         </div>
-      </form>
-    </Container>
-  </div>
-);
+      </Container>
+    ) : (
+      <div>
+        <p>Details about the Negotiation - Master Owner...</p>
+        <Container className="mt-5">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="priceInput">Price</label>
+              <input
+                type="number"
+                className="form-control"
+                id="priceInput"
+                placeholder="Enter aside price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)} // Update state on input change
+              />
+              {showError && <div className="text-danger">Please enter a price.</div>} {/* Error message */}
+            </div>
+            <button type="submit" className="btn btn-primary mt-3">Confirm Price</button>
+          </form>
+        </Container>
+    </div> 
+    )
+  );
+};
 
 export default NegoMasterContent;
